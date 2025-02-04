8ed9efcce0d75eae9b64b18b9e5cbdf4bcaa004f
ran4525 <169119771+ran4525@users.noreply.github.com>
2025-02-04 19:54:40 +0800

    SearchPage.vue

--- views/SearchPage.vue
+++ views/SearchPage.vue
@@ -9,26 +9,6 @@
           </el-submenu>
           <el-submenu index="2">
             <template slot="title"><i class="el-icon-menu"></i><el-link :underline="false">账单明细</el-link></template>
-            <el-menu-item-group>
-              <el-menu-item index="2-1">年</el-menu-item>
-              <el-menu-item index="2-2">月</el-menu-item>
-              <el-menu-item index="2-3">日</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title"><i class="el-icon-setting"></i>设置</template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="3-1">选项1</el-menu-item>
-              <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
           </el-submenu>
         </el-menu>
       </el-aside>
